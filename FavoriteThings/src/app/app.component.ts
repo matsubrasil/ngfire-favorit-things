d3e61f710bef1c962199cb9f228801b0c2a64915
matsubrasil <matsuboy@outlook.com>
2017-06-24 17:33:21 -0300

    only local favorite color

--- FavoriteThings/src/app/app.component.ts
+++ FavoriteThings/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title= 'MatsuBrasil Favorite Things';
+  favoriteColor= 'tomato';
+
+  setColor(selectedColor: string): void {
+    this.favoriteColor = selectedColor;
+  }
+
+  updateColor(): void {
+    console.log( 'TODO: update the color' );
+  }
 }
